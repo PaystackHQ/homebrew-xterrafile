01dfbcfa68167b6bd3e7798dcafa2d79dccdb993
pysysops <tim.birkett@devopsmakers.com>
2019-03-03 16:37:20 +0700

    Brew formula update for xterrafile version v0.3.5

--- Formula/xterrafile.rb
+++ Formula/xterrafile.rb
@@ -2,9 +2,9 @@
 class Xterrafile < Formula
   desc "Systematically manage external modules from Github for use in Terraform."
   homepage "https://github.com/devopsmakers/xterrafile"
-  url "https://github.com/devopsmakers/xterrafile/releases/download/v0.3.4/xterrafile_0.3.4_Darwin_x86_64.tar.gz"
-  version "0.3.4"
-  sha256 "7838d19fd4e213ab64735ee885a582480bb799edd7a086536836b10e92d89f78"
+  url "https://github.com/devopsmakers/xterrafile/releases/download/v0.3.5/xterrafile_0.3.5_Darwin_x86_64.tar.gz"
+  version "0.3.5"
+  sha256 "a93c1fa6901b1b06d45a404bcc7b7677d6eb597870f80c415f71d5b9c0073cb2"
 
   def install
     bin.install "xterrafile"
